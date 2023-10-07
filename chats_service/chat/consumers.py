35a527a5c9ca343f8c7ceb1b5fb30d47fd392ed7
zormnus <zotov.max17@mail.ru>
2023-10-07 16:02:30 +0300

    unused code deleted

--- chats_service/chat/consumers.py
+++ chats_service/chat/consumers.py
@@ -11,13 +11,16 @@ from .models import Message, Room
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
-        self.room_group_name = f"chat_{self.room_name}"
-        self.bd_room = await self.get_room(self.room_name)
+        try:
+            self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
+            self.room_group_name = f"chat_{self.room_name}"
+            self.bd_room = await self.get_room(self.room_name)
 
-        await self.channel_layer.group_add(self.room_group_name,
-                                           self.channel_name)
-        await self.accept()
+            await self.channel_layer.group_add(self.room_group_name,
+                                               self.channel_name)
+            await self.accept()
+        except Http404:
+            print(f"{self.room_name} connection error (Can't find that room)")
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_group_name,
@@ -47,11 +50,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     @sync_to_async
     def get_room(self, room_id):
-        return Room.objects.get(uuid=room_id)
-
-    @sync_to_async
-    def get_user_id(self, username) -> User:
-        return User.objects.get(username=username)
+        return get_object_or_404(Room, uuid=room_id)
 
     @sync_to_async
     def room_extend(self, message):
