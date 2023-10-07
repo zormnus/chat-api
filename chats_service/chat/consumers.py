30a261d29b53b3fb6bb3bd92d6252ab8aa62e16a
zormnus <zotov.max17@mail.ru>
2023-10-07 20:47:04 +0300

    unused imports organize

--- chats_service/chat/consumers.py
+++ chats_service/chat/consumers.py
@@ -1,6 +1,5 @@
 import json
 
-from asgiref.sync import sync_to_async
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 from django.contrib.auth.models import User
